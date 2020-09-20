d54317c73a447bc19a1b72bb0b77ef64b6cac1f2
m431001 <m431001@AFR691170.france.ad.airfrance.fr>
2020-09-21 00:56:11 +0200

    3.1.0

--- demo-root/demo-rest-client/src/main/java/org/sklsft/demo/rest/client/RestClientConfig.java
+++ demo-root/demo-rest-client/src/main/java/org/sklsft/demo/rest/client/RestClientConfig.java
@@ -16,8 +16,6 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -63,8 +61,8 @@ public class RestClientConfig {
 		loggerInterceptor.setTraceSentBody(Boolean.valueOf(env.getProperty("accessLog.traceSentBody")));
 		loggerInterceptor.setTraceReceivedBody(Boolean.valueOf(env.getProperty("accessLog.traceReceivedBody")));
 		interceptors.add(loggerInterceptor);
-		result.setInterceptors(interceptors);
-		
+		result.setInterceptors(interceptors);		
+				
 		return result;
 	}
 	
